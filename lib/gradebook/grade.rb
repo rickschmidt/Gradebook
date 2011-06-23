7adfb1423112d6421e9bb14d3446bf65bfdc16a7
Rick Schmidt <rickschmidt@mbp.gateway.2wire.net>
2011-06-23 10:53:14 -0500

    Added enter grade method to grade class

--- lib/gradebook/grade.rb
+++ lib/gradebook/grade.rb
@@ -6,7 +6,7 @@ require 'gradebook/utility'
 module Gradebook
    class Grade
      
-     def self.grade_each_student(list_feed,column_id)
+     def self.grade_each_student(sps_client,sps_id,list_feed,column_id)
         list_feed.elements.each('entry') do |entry| 
           student_name=''
            entry.elements.each('gsx:name') do |name|
@@ -15,15 +15,27 @@ module Gradebook
            end
            puts "Students Name: #{student_name.text}"
            puts "Enter Grade for #{student_name.text}:"
-            self.enter_grade
+            self.enter_grade(sps_client,sps_id,entry,column_id)
         end
 
      end
    
    
-    def self.enter_grade
-      
-      
+     def self.enter_grade(sps_client,sps_id,entry,column_id)
+         edit_uri=''
+           entry.add_namespace('http://www.w3.org/2005/Atom')
+            entry.add_namespace('gd','http://schemas.google.com/g/2005')
+            entry.add_namespace('gs','http://schemas.google.com/spreadsheets/2006')
+            entry.add_namespace('gsx','http://schemas.google.com/spreadsheets/2006/extended')
+         entry.elements.each('link') do |link|
+             edit_uri = entry.elements["link[@rel='edit']"].attributes['href']
+             puts edit_uri
+         end
+         entry.elements.each("gsx:*[#{column_id}]") do |col|
+             col.text="C"
+         end
+         #elements['gs:colCount'].text = "#{total_columns.to_i}"
+         sps_client.put(edit_uri,entry.to_s)
     end
    
    
--- spec/grade_spec.rb
+++ spec/grade_spec.rb
@@ -18,14 +18,15 @@ describe "Grade" do
        
        it "should be able to search for a category in a header row and extract the column ID ie the key of the header row hash" do
          headers=Search.get_columns_headers(@client.sps_client,@sps_id)
-         category_id=Search.search_for_column_id("Participation",headers)
+         column_id=Search.search_for_column_id("HW3",headers)
+         puts column_id
        end
        
        it "should ask for the category_id when the grade command is issued" do
          headers=Search.get_columns_headers(@client.sps_client,@sps_id)
-         column_id=Search.search_for_column_id('Participation',headers)
+         column_id=Search.search_for_column_id('Q1',headers)
          list_feed=Utility.get_list_feed(@client.sps_client,@sps_id)
-         Grade.grade_each_student(list_feed,column_id)
+         Grade.grade_each_student(@client.sps_client,@sps_id,list_feed,column_id)
        end
        
        
